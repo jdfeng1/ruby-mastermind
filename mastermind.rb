21cf64c139bb4ed5020fd7e2d396fa3ccdbb284c
jdfeng1 <justinfeng64@gmail.com>
2022-09-12 17:59:56 -0500

    Create file and outline classes and modules

---
+++ mastermind.rb
@@ -0,0 +1,27 @@
+class Player
+
+end
+
+class Human < Player
+
+end
+
+class Computer < Player
+
+end
+
+class GameBoard
+
+end
+
+module CodeBreaker
+
+end
+
+module CodeMaker
+
+end
+
+class GameExecute
+
+end
