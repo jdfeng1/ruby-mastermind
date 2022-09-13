09432ff770bd71ef62cde6fac9b928161e905f48
jdfeng1 <justinfeng64@gmail.com>
2022-09-12 19:00:39 -0500

    Separate text module and create intro selection

---
+++ display.rb
@@ -0,0 +1,8 @@
+module Display
+  def introText
+    puts "Let's play Mastermind\n\n"
+    puts "In this game, a code breaker attempts to discover a 4-digit code created by the code maker.\nEach turn, the code maker will reveal clues as to the correct numbers:\nX if there is a correct number in it's correct position\nO if there is a correct number in a wrong position\n"
+    puts "\nChoose: Do you want to be CODE BREAKER or CODE MAKER?\n"
+    puts "Press [1] for BREAKER\nPress [2] for MAKER\n"
+  end
+end
--- mastermind.rb
+++ mastermind.rb
@@ -1,27 +1,41 @@
-class Player
 
+require_relative "./display.rb"
+
+class Player
+  
 end
 
 class Human < Player
-
+  
 end
 
 class Computer < Player
 
 end
 
-class GameBoard
-
-end
-
-module CodeBreaker
-
-end
-
-module CodeMaker
-
-end
-
 class GameExecute
+include Display
+  def initialize
+    @guesses_left = 12
+    @code = nil
+  end
+
+  def startGame
+    introText
+    choice = loop do
+      currentChoice = gets.chomp
+      break currentChoice if["1","2"].include? currentChoice
+      puts "Please choose 1 or 2"
+    end
+    p choice
+    createPlayers(choice)
+  end
+  
+  def createPlayers(playerChoice)
+    
+  end
 
 end
+
+newGame = GameExecute.new
+newGame.startGame
